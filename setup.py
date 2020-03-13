59c05ef25328d1463ab2bfdf80424905a377a68e
James Chien <shchien@dt42.io>
2020-03-13 13:49:36 +0800

    Bump version to v0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='py-threads-client',
-    version='0.0.2',
+    version='0.0.3',
     description='Python binding of Threads V2 client',
     long_description=long_description,
     long_description_content_type='text/markdown',
